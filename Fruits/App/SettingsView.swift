51a845f61e1322922ab1bea049c04105359eebf9
Daniel Lopes <daniel.dxdb@gmail.com>
2022-05-24 10:44:42 -0300

    Add application info

--- Fruits/App/SettingsView.swift
+++ Fruits/App/SettingsView.swift
@@ -38,6 +38,17 @@ struct SettingsView: View {
                     // MARK: - SECTION 2
                     
                     // MARK: - SECTION 3
+                    GroupBox(
+                        label: SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")
+                    ) {                        
+                        SettingsRowView(name: "Developer", content: "Daniel Lopes")
+                        SettingsRowView(name: "Designer", content: "Robert Petras")
+                        SettingsRowView(name: "Compatibility", content: "iOS 15")
+                        SettingsRowView(name: "Website", linkLabel: "TechnoLopes", linkDestination: "technolopes.com")
+                        SettingsRowView(name: "Instagram", linkLabel: "@technolopes", linkDestination: "instagram.com/technolopes")
+                        SettingsRowView(name: "SwiftUI", content: "2.0")
+                        SettingsRowView(name: "Version", content: "1.1.0")
+                    }
                     
                 } //: VSTACK
                 .navigationBarTitle(Text("Settings"), displayMode: .large)
