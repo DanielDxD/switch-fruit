88e921286cf1c3d52d20eb1bf03db3c2e60ff18b
Daniel Lopes <daniel.dxdb@gmail.com>
2022-05-24 10:25:13 -0300

    Creating settings page

---
+++ Fruits/View/SettingsLabelView.swift
@@ -0,0 +1,33 @@
+//
+//  SettingsLabelView.swift
+//  Fruits
+//
+//  Created by Daniel Lopes on 23/05/22.
+//
+
+import SwiftUI
+
+struct SettingsLabelView: View {
+    // MARK: - PROPERTIES
+    var labelText: String
+    var labelImage: String
+    
+    // MARK: - BODY
+    var body: some View {
+        HStack {
+            Text(labelText.uppercased())
+                .fontWeight(.bold)
+            Spacer()
+            Image(systemName: labelImage)
+        }
+    }
+}
+
+// MARK: - PREVIEW
+struct SettingsLabelView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsLabelView(labelText: "Fruits", labelImage: "info.circle")
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
